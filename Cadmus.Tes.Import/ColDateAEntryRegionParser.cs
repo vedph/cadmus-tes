9552886ed3c658b52dc14baf6e4574536a71b21a
Myrmex <dfusi@hotmail.com>
2026-07-25 10:42:11 +0200

    Adding parsers

---
+++ Cadmus.Tes.Import/ColDateAEntryRegionParser.cs
@@ -0,0 +1,67 @@
+﻿using Cadmus.Import.Proteus;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Tes.Import;
+
+/// <summary>
+/// TES column date A entry region parser. This just caches the A term for
+/// later use (when dealing with B).
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+[Tag("entry-region-parser.tes.col-date-a")]
+public sealed class ColDateAEntryRegionParser :
+    EntryRegionParser, IEntryRegionParser
+{
+    /// <summary>
+    /// Gets the tags of the regions that this parser can handle.
+    /// </summary>
+    public string[] RegionTags => ["col-date-a"];
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="entryRegions" />.
+    /// </summary>
+    /// <param name="entrySet">The entries set.</param>
+    /// <param name="entryRegions">The regions.</param>
+    /// <param name="entryRegionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    protected override int DoParse(EntrySet entrySet, int entryIndex,
+        IReadOnlyList<EntryRegion> entryRegions, int entryRegionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(entrySet);
+        ArgumentNullException.ThrowIfNull(entryRegions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)entrySet.Context;
+        EntryRegion region = entryRegions[entryRegionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            Logger?.LogError("col-date-a column without any item at region {Region}",
+                region);
+            throw new InvalidOperationException(
+                "col-date-a column without any item at region " + region);
+        }
+
+        DecodedTextEntry txt = entrySet.GetEntryAt<DecodedTextEntry>(
+            entryIndex + 1)!;
+        string? value = ImportHelper.FilterValue(txt.Value, false);
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            ctx.Data["col-date-a"] = value;
+            Logger?.LogInformation("col-date-a column value {Value} cached",
+                value);
+        }
+
+        return entryIndex + 3;
+    }
+}
