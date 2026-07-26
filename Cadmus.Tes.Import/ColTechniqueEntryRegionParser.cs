80a282d0625771e028f7054dcafb544a8571187e
Myrmex <dfusi@hotmail.com>
2026-07-26 15:33:47 +0200

    Technique

---
+++ Cadmus.Tes.Import/ColTechniqueEntryRegionParser.cs
@@ -0,0 +1,77 @@
+﻿using Cadmus.Epigraphy.Parts;
+using Cadmus.Import.Proteus;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Tes.Import;
+
+/// <summary>
+/// TES column execution type 1 and 2 entry region parser.
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+[Tag("entry-region-parser.tes.col-technique")]
+public sealed class ColTechniqueEntryRegionParser :
+    EntryRegionParser, IEntryRegionParser
+{
+    /// <summary>
+    /// Gets the tags of the regions that this parser can handle.
+    /// </summary>
+    public string[] RegionTags => ["col-execution_type_1", "col-execution_type_2"];
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="entryRegions" />.
+    /// </summary>
+    /// <param name="entrySet">The entries set.</param>
+    /// <param name="entryRegions">The regions.</param>
+    /// <param name="entryRegionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    protected override int DoParse(EntrySet entrySet, int entryIndex,
+        IReadOnlyList<EntryRegion> entryRegions, int entryRegionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(entrySet);
+        ArgumentNullException.ThrowIfNull(entryRegions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)entrySet.Context;
+        EntryRegion region = entryRegions[entryRegionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            Logger?.LogError("{Tag} column without any item at region {Region}",
+                region.Tag, region);
+            throw new InvalidOperationException(
+                $"{region.Tag} column without any item at region {region}");
+        }
+
+        DecodedTextEntry txt = entrySet.GetEntryAt<DecodedTextEntry>(
+            entryIndex + 1)!;
+        string? value = ImportHelper.FilterValue(txt.Value, false);
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            string id = ImportHelper.GetThesaurusId(ctx, region,
+                "epi-technique-types", value, Logger);
+            if (id == null)
+            {
+                Logger?.LogError("Unknown execution type label for {Tag}: " +
+                    "\"{Label}\" at region {Region}", region.Tag, value, region);
+            }
+            else
+            {
+                EpiTechniquePart part =
+                    ctx.EnsurePartForCurrentItem<EpiTechniquePart>();
+                part.Techniques.Add(id);
+            }
+        }
+
+        return entryIndex + 3;
+    }
+}
