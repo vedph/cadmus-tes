392fb04b2ab0b4a78631af0446b03ba62b72eb5f
Myrmex <dfusi@hotmail.com>
2026-07-26 15:40:17 +0200

    Repository and inventory

---
+++ Cadmus.Tes.Import/ColRepositoryEntryRegionParser.cs
@@ -0,0 +1,79 @@
+﻿using Cadmus.General.Parts;
+using Cadmus.Import.Proteus;
+using Fusi.Tools.Configuration;
+using Microsoft.Extensions.Logging;
+using Proteus.Core.Entries;
+using Proteus.Core.Regions;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Tes.Import;
+
+/// <summary>
+/// TES column repository and inventory entry regions parser.
+/// </summary>
+/// <seealso cref="EntryRegionParser" />
+/// <seealso cref="IEntryRegionParser" />
+/// <remarks>
+/// Initializes a new instance of the <see cref="ColRepositoryEntryRegionParser"/>
+/// class.
+/// </remarks>
+/// <param name="logger">The logger.</param>
+[Tag("entry-region-parser.tes.col-repository")]
+public sealed class ColRepositoryEntryRegionParser :
+    EntryRegionParser, IEntryRegionParser
+{
+    /// <summary>
+    /// Gets the tags of the regions that this parser can handle.
+    /// </summary>
+    public string[] RegionTags => ["col-repository_name", "col-inventory_number"];
+
+    /// <summary>
+    /// Parses the region of entries at <paramref name="regionIndex" />
+    /// in the specified <paramref name="entryRegions" />.
+    /// </summary>
+    /// <param name="entrySet">The entries set.</param>
+    /// <param name="entryRegions">The regions.</param>
+    /// <param name="entryRegionIndex">Index of the region in the set.</param>
+    /// <returns>
+    /// The index to the next region to be parsed.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">set or regions</exception>
+    protected override int DoParse(EntrySet entrySet, int entryIndex,
+        IReadOnlyList<EntryRegion> entryRegions, int entryRegionIndex)
+    {
+        ArgumentNullException.ThrowIfNull(entrySet);
+        ArgumentNullException.ThrowIfNull(entryRegions);
+
+        CadmusEntrySetContext ctx = (CadmusEntrySetContext)entrySet.Context;
+        EntryRegion region = entryRegions[entryRegionIndex];
+
+        if (ctx.CurrentItem == null)
+        {
+            Logger?.LogError("{Tag} column without any item at region {Region}",
+                region.Tag, region);
+            throw new InvalidOperationException(
+                $"{region.Tag} column without any item at region {region}");
+        }
+
+        DecodedTextEntry txt = entrySet.GetEntryAt<DecodedTextEntry>(
+            entryIndex + 1)!;
+
+        string? value = ImportHelper.FilterValue(txt.Value, false);
+
+        if (!string.IsNullOrEmpty(value))
+        {
+            // metadata
+            MetadataPart part = ctx.EnsurePartForCurrentItem<MetadataPart>();
+            part.Metadata.Add(new Metadatum
+            {
+                Type = "string",
+                Name = region.Tag == "col-repository_name"
+                    ? "preservation-place" : "inventory-nr",
+                Value = value
+            });
+        }
+
+        return entryIndex + 3;
+    }
+}
